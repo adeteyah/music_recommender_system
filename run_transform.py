a7ae8453aecda69c21d2ba835a59099e1e19e6bc
Adeteyah <adeteyah@gmail.com>
2024-07-17 18:41:37 +0000

    Jul 18, 2024, 1:41 AM

--- run_transform.py
+++ run_transform.py
@@ -38,50 +38,7 @@ def insert_data_into_db(db_path, table_name, data):
     conn.close()
 
 
-def fill_playlist_db():
-    for filename in os.listdir(transformed_path):
-        if filename.endswith('.csv'):
-            file_path = os.path.join(transformed_path, filename)
-            creator_id, playlist_id = filename.split(
-                '_')[0], filename.split('_')[1].split('.')[0]
-
-            df = pd.read_csv(file_path)
-
-            track_data = [
-                {
-                    "track_id": row['spotify_id'],
-                    "track_name": "",
-                    "artist_ids": ",".join(row['artists_id'].split(','))
-                }
-                for _, row in df.iterrows()
-            ]
-            insert_data_into_db(songs_db_path, 'tracks', track_data)
-
-            artists = set()
-            for _, row in df.iterrows():
-                for artist_id in row['artists_id'].split(','):
-                    artists.add(artist_id)
-
-            artist_data = [
-                {
-                    "artist_id": artist_id,
-                    "artist_name": "",
-                    "artist_genres": ""
-                }
-                for artist_id in artists
-            ]
-            insert_data_into_db(songs_db_path, 'artists', artist_data)
-
-            playlist_data = {
-                "creator_id": creator_id,
-                "playlist_id": playlist_id,
-                "playlist_track_count": len(df)
-            }
-            insert_data_into_db(playlists_db_path,
-                                'playlists', [playlist_data])
-
-
-def fill_songs_db_with_spotify():
+def fill_artists_table():
     conn = sqlite3.connect(songs_db_path)
     cursor = conn.cursor()
 
@@ -108,6 +65,14 @@ def fill_songs_db_with_spotify():
         time.sleep(5)
         conn.commit()
 
+    conn.close()
+    print("Finished updating artists with Spotify data.")
+
+
+def fill_tracks_table():
+    conn = sqlite3.connect(songs_db_path)
+    cursor = conn.cursor()
+
     # Updating tracks
     while True:
         cursor.execute("SELECT * FROM tracks WHERE track_name = '' LIMIT 5")
@@ -129,26 +94,21 @@ def fill_songs_db_with_spotify():
         conn.commit()
 
     conn.close()
-    print("Finished updating tracks and artists with Spotify data.")
+    print("Finished updating tracks with Spotify data.")
 
 
 def choose_process():
     while True:
         print("\nChoose a process to run:")
-        print("1. Fill playlist database")
-        print("2. Fill songs database with Spotify data")
+        print("1. Fill artists table")
+        print("2. Fill tracks table")
         print("3. Exit")
         choice = input("Enter your choice (1 or 2): ")
 
         if choice == '1':
-            fill_playlist_db()
-            print("Playlist DB filled with current data.")
+            fill_artists_table()
         elif choice == '2':
-            fill_songs_db_with_spotify()
-            print("Songs DB filled with Spotify data.")
-        elif choice == '3':
-            print("Exiting the program.")
-            break
+            fill_tracks_table()
         else:
             print("Invalid choice. Please enter '1' or '2'.")
 
