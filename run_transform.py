365d0ff6fee3ba4b8e9db5ea86ef816de4646a21
Adeteyah <adeteyah@gmail.com>
2024-07-17 13:28:52 +0000

    Jul 17, 2024, 8:28 PM

--- run_transform.py
+++ run_transform.py
@@ -145,6 +145,7 @@ def fill_songs_db_with_spotify():
                 track_name = track_info['name']
                 cursor.execute(
                     "UPDATE tracks SET track_name = ? WHERE track_id = ?", (track_name, track_id))
+                print(f'Updated track name for {track_info}: {track_name}')
             except SpotifyException as e:
                 print(f"Error fetching track {track_id}: {e}")
 
@@ -166,6 +167,8 @@ def fill_songs_db_with_spotify():
                 artist_genres = ",".join(artist_info['genres'])
                 cursor.execute("UPDATE artists SET artist_name = ?, artist_genres = ? WHERE artist_id = ?",
                                (artist_name, artist_genres, artist_id))
+                print(f'Updated artist name, genres for {
+                      artist_info}: {artist_name}, {artist_genres}')
             except SpotifyException as e:
                 print(f"Error fetching artist {artist_id}: {e}")
 
