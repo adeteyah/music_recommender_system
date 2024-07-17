a1a44bf3c6165dc5cccbc41edc80b8de9fcffd9b
Adeteyah <adeteyah@gmail.com>
2024-07-17 17:10:02 +0000

    Jul 18, 2024, 12:10 AM

--- scripts/rs/cf.py
+++ scripts/rs/cf.py
@@ -20,6 +20,34 @@ def cf_result(ids):
     conn_songs = sqlite3.connect(db_songs)
     cursor_songs = conn_songs.cursor()
 
+    # Retrieve track and artist information for inputted IDs
+    input_ids_info = []
+    for track_id in ids:
+        cursor_songs.execute(
+            "SELECT track_name, artist_ids FROM tracks WHERE track_id=?", (track_id,))
+        track_row = cursor_songs.fetchone()
+        if track_row:
+            track_name, artist_ids = track_row
+            if not track_name:
+                track_name = "(Unknown)"
+            artist_names = []
+            for artist_id in artist_ids.split(', '):
+                cursor_songs.execute(
+                    "SELECT artist_name FROM artists WHERE artist_id=?", (artist_id,))
+                artist_row = cursor_songs.fetchone()
+                if artist_row:
+                    artist_name = artist_row[0]
+                    if not artist_name:
+                        artist_name = "(Unknown)"
+                    artist_names.append(artist_name)
+            if not artist_names:
+                artist_names.append("(Unknown)")
+            input_ids_info.append({
+                'track_id': track_id,
+                'track_name': track_name,
+                'artists': ', '.join(artist_names)
+            })
+
     # Find all playlists that contain the given track IDs
     query = "SELECT playlist_id, playlist_items FROM items"
     cursor_playlist.execute(query)
@@ -37,7 +65,7 @@ def cf_result(ids):
                     track_counts[item] = 0
                 track_counts[item] += 1
 
-    # Retrieve track and artist information from the songs database
+    # Retrieve track and artist information for all tracks
     track_info = {}
     for track_id in track_counts.keys():
         cursor_songs.execute(
@@ -77,7 +105,13 @@ def cf_result(ids):
         playlist_contributions.items(), key=lambda item: item[1], reverse=True)
 
     # Format the results
-    results = "Playlist Contributed\n"
+    results = "Inputted IDS\n"
+    for idx, track in enumerate(input_ids_info, 1):
+        track_url = f"https://open.spotify.com/track/{track['track_id']}"
+        results += f"{idx}. {track['artists']
+                             } - {track['track_name']} [{track_url}]\n"
+
+    results += "\nPlaylist Contributed\n"
     rank = 1
     for playlist_id, count in sorted_playlists:
         playlist_url = f"https://open.spotify.com/playlist/{playlist_id}"
