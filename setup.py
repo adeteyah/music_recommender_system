d7dd7954758f80226d7545a0cd61995fc9cd9879
Adeteyah <adeteyah@gmail.com>
2024-07-17 08:10:49 +0000

    Jul 17, 2024, 3:10 PM

--- setup.py
+++ setup.py
@@ -45,11 +45,16 @@ CREATE TABLE IF NOT EXISTS artists (
     artist_name TEXT,
     artist_genres TEXT
 );
+
+CREATE INDEX IF NOT EXISTS idx_artists_artist_id ON artists (artist_id);
+
 CREATE TABLE IF NOT EXISTS tracks (
     track_id TEXT PRIMARY KEY,
     track_name TEXT,
     artist_ids TEXT
 );
+
+CREATE INDEX IF NOT EXISTS idx_tracks_track_id ON tracks (track_id);
 """
 
 playlist_schema = """
@@ -57,6 +62,9 @@ CREATE TABLE IF NOT EXISTS playlists (
     playlist_id TEXT PRIMARY KEY,
     creator_id TEXT
 );
+
+CREATE INDEX IF NOT EXISTS idx_playlist_id ON playlists (playlist_id);
+CREATE INDEX IF NOT EXISTS idx_creator_id ON playlists (creator_id);
 """
 
 create_database("data/db/songs_details.db", songs_schema)
