9e5ad8ef461c17ab44f20e3361c87c0c7be73d47
Adeteyah <adeteyah@gmail.com>
2024-07-17 14:18:02 +0000

    Jul 17, 2024, 9:18 PM

--- scripts/rs/cf.py
+++ scripts/rs/cf.py
@@ -1,46 +1,44 @@
 import os
 import csv
-
 import configparser
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
 
-def load_data(directory_path):
-    data = []
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".csv"):
-            file_path = os.path.join(directory_path, filename)
-            with open(file_path, 'r', newline='') as csvfile:
-                csvreader = csv.reader(csvfile)
-                for row in csvreader:
-                    # You can adjust how you store each row based on your specific needs
-                    data.append(row)
-    return data
-
-
-def generate_recommendations(playlists, ids):
-    matched_playlists = []
-    for playlist in playlists:
-        if playlist.get('spotify_id') in ids:
-            matched_playlists.append(playlist)
-
-    return matched_playlists
-
-
-def write_recommendations():
-    pass
-
-
 def cf_result(ids):
-    return print(f"Stored result on {ids}!")
+    def load_data(directory_path):
+        data = []
+        for filename in os.listdir(directory_path):
+            if filename.endswith(".csv"):
+                file_path = os.path.join(directory_path, filename)
+                with open(file_path, 'r', newline='') as csvfile:
+                    csvreader = csv.reader(csvfile)
+                    for row in csvreader:
+                        data.append(row)
+        return data
 
+    def generate_recommendations(playlists, ids):
+        matched_playlists = []
+        for playlist in playlists:
+            if playlist.get('spotify_id') in ids:
+                matched_playlists.append(playlist)
+        return matched_playlists
 
-playlists_data = load_data(config['dir']['transformed'])
-print(f"Loaded {len(playlists_data)} rows of data from playlists CSV.")
+    def write_recommendations():
+        pass
+
+    playlists = load_data(config['dir']['transformed'])
+    print(f"Loaded {len(playlists)} rows of data from playlists CSV.")
+    matched_playlists = generate_recommendations(playlists, ids)
+    for playlist in matched_playlists:
+        print(f"Matched playlist: {
+              playlist['name']} (Spotify ID: {playlist['spotify_id']})")
+
+    return f"Stored result on {ids}!"
 
 
 if __name__ == "__main__":
     ids = ['1uCDg9WDXzG5j1tVqnFNBR']
-    cf_result(ids)
+    result = cf_result(ids)
+    print(result)
