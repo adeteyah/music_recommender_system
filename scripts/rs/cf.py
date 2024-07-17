0552d5716cde80afecc31ca493db3f1337751c4f
Adeteyah <adeteyah@gmail.com>
2024-07-17 14:06:46 +0000

    Jul 17, 2024, 9:06 PM

--- scripts/rs/cf.py
+++ scripts/rs/cf.py
@@ -1,5 +1,23 @@
-def load_data():
-    pass
+import os
+import csv
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
+
+
+def load_data(directory_path):
+    data = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(directory_path, filename)
+            with open(file_path, 'r', newline='') as csvfile:
+                csvreader = csv.reader(csvfile)
+                for row in csvreader:
+                    # You can adjust how you store each row based on your specific needs
+                    data.append(row)
+    return data
 
 
 def generate_recommendations():
@@ -13,6 +31,9 @@ def write_recommendations():
 def cf_result(ids):
     return print(f"Getting recommendation from {ids}!")
 
+
+all_data = load_data(config['dir']['transformed'])
+print(f"Loaded {len(all_data)} rows of data from CSV files.")
 
 if __name__ == "__main__":
     ids = ['1uCDg9WDXzG5j1tVqnFNBR']
