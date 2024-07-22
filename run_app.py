972c80fcc569dfefcff87751327510b42e576f89
Adeteyah <adeteyah@gmail.com>
2024-07-22 04:03:46 +0000

    Jul 22, 2024, 11:03 AM

--- run_app.py
+++ run_app.py
@@ -4,6 +4,10 @@ from scripts.rs import cbf
 from scripts.rs import cf
 from scripts.rs import hf_cbf_cf
 from scripts.rs import hf_cf_cbf
+from scripts.rs_optimized import cbf
+from scripts.rs_optimized import cf
+from scripts.rs_optimized import hf_cbf_cf
+from scripts.rs_optimized import hf_cf_cbf
 
 ids = [
     '26WgejlZUG6TxLo8djVxUp',
@@ -12,6 +16,32 @@ ids = [
     '7bWHFauPVNfMYUVZnAXbo9',
     '2iOgTijjnxjtz7723Xs4sp'
 ]
+
+print("\n# Generating CBF Result")
+start = time.time()
+cbf.cbf_result(ids)
+end = time.time()
+print("CBF execution time: ", end - start)
+
+print("\n# Generating CF Result")
+start = time.time()
+cf.cf_result(ids)
+end = time.time()
+print("CF execution time: ", end - start)
+
+print("\n# Generating HF-CBF-CF Result")
+start = time.time()
+hf_cbf_cf.hfcbfcf_result(ids)
+end = time.time()
+print("HFCBFCF execution time: ", end - start)
+
+print("\n# Generating HF-CF-CBF Result")
+start = time.time()
+hf_cf_cbf.hfcfcbf_result(ids)
+end = time.time()
+print("HFCFCBF execution time: ", end - start, "\n")
+
+print("\n# Generating CBF Result")
 
 print("\n# Generating CBF Result")
 start = time.time()
