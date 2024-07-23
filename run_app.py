5d04be0fe8e9f7deb1334762bd2f266acb343659
Adeteyah <adeteyah@gmail.com>
2024-07-23 15:38:29 +0000

    Jul 23, 2024, 10:38 PM

--- run_app.py
+++ run_app.py
@@ -5,10 +5,6 @@ from scripts.rs import cbf
 from scripts.rs import cf
 from scripts.rs import hf_cbf_cf
 from scripts.rs import hf_cf_cbf
-# from scripts.rs_optimized import o_cbf
-# from scripts.rs_optimized import o_cf
-# from scripts.rs_optimized import o_hf_cbf_cf
-# from scripts.rs_optimized import o_hf_cf_cbf
 
 seed.fill_playlists_table()
 
@@ -39,29 +35,3 @@ start = time.time()
 hf_cf_cbf.hfcfcbf_result(ids)
 end = time.time()
 print("HFCFCBF execution time: ", end - start, "\n")
-
-# print('=' * 100)
-
-# print("\n# Generating Optimized CBF Result")
-# start = time.time()
-# o_cbf.o_cbf_result(ids)
-# end = time.time()
-# print("CBF execution time: ", end - start)
-
-# print("\n# Generating Optimized CF Result")
-# start = time.time()
-# o_cf.o_cf_result(ids)
-# end = time.time()
-# print("CF execution time: ", end - start)
-
-# print("\n# Generating Optimized HF-CBF-CF Result")
-# start = time.time()
-# o_hf_cbf_cf.o_hfcbfcf_result(ids)
-# end = time.time()
-# print("HFCBFCF execution time: ", end - start)
-
-# print("\n# Generating Optimized HF-CF-CBF Result")
-# start = time.time()
-# o_hf_cf_cbf.o_hfcfcbf_result(ids)
-# end = time.time()
-# print("HFCFCBF execution time: ", end - start, "\n")
