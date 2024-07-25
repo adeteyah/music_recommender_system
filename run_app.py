b305a70d9d6aa4fd05e29dd219e383bba3a3353c
Adeteyah <adeteyah@gmail.com>
2024-07-25 08:14:47 +0700

    FINAL ALL RS

--- run_app.py
+++ run_app.py
@@ -5,6 +5,7 @@ from scripts.rs import cbf
 from scripts.rs import cbf_o
 from scripts.rs import cf
 from scripts.rs import hf_cbf_cf
+from scripts.rs import hf_cbf_cf_o
 from scripts.rs import hf_cf_cbf
 
 seed.fill_playlists_table()
@@ -24,35 +25,36 @@ ids = [
     '4xNXqEBx1VXuph56zyUzBg',
 ]
 
-# CBF
 print("\n# Generating CBF Result")
 start = time.time()
 cbf.cbf_result(ids)
 end = time.time()
 print("CBF execution time: ", end - start)
 
-# Optimized by Normalization
-print("\n# Generating Optimized CBF Result")
+print("\n# Generating CBF (Optimized) Result")
 start = time.time()
 cbf_o.o_cbf_result(ids)
 end = time.time()
 print("CBF execution time: ", end - start)
 
-# CF
 print("\n# Generating CF Result")
 start = time.time()
 cf.cf_result(ids)
 end = time.time()
 print("CF execution time: ", end - start)
 
-# HF CBF CF (From O_CBF Combined with ???)
 print("\n# Generating HF-CBF-CF Result")
 start = time.time()
 hf_cbf_cf.hfcbfcf_result(ids)
 end = time.time()
 print("HFCBFCF execution time: ", end - start)
 
-# HF CF CBF (From CF Combined with Euclidean Distance)
+print("\n# Generating HF-CBF-CF (Optimized) Result")
+start = time.time()
+hf_cbf_cf_o.o_hfcbfcf_result(ids)
+end = time.time()
+print("HFCBFCF execution time: ", end - start)
+
 print("\n# Generating HF-CF-CBF Result")
 start = time.time()
 hf_cf_cbf.hfcfcbf_result(ids)
