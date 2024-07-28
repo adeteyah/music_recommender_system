439d1d89c9510c4a65721f740f359f741a314189
Adeteyah <adeteyah@gmail.com>
2024-07-28 05:26:59 +0000

    Jul 28, 2024, 12:26 PM

--- scripts/seed/fill_tracks_lyrics.py
+++ scripts/seed/fill_tracks_lyrics.py
@@ -90,13 +90,10 @@ def get_lyrics_from_url(url):
 
 
 def clean_lyrics(lyrics):
-    # Remove bracketed sections and replace with spaces
+    # Remove bracketed sections and replace with a space
     lyrics = re.sub(r'\[.*?\]', ' ', lyrics)
-    # Replace multiple spaces with a single space
+    # Replace multiple spaces with a single space to clean up any extra spaces left by the replacement
     lyrics = re.sub(r'\s+', ' ', lyrics)
-    # Ensure there's a space between words and the start or end of a bracketed section
-    lyrics = re.sub(r'(?<=\w)(?=\[)', ' ', lyrics)
-    lyrics = re.sub(r'(?<=\])(?!\s)', ' ', lyrics)
     return lyrics.strip()
 
 
