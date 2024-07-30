112e6fe426e82b79181c35b6a7be7eae9f7b6724
Adeteyah <adeteyah@gmail.com>
2024-07-30 18:41:44 +0000

    Jul 31, 2024, 1:41 AM

--- scripts/db/fill.py
+++ scripts/db/fill.py
@@ -1,246 +1,104 @@
 import sqlite3
 import configparser
-from collections import Counter
 
+# Read config file
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
 songs_db_path = config['db']['songs_db']
 playlists_db_path = config['db']['playlists_db']
 
+# Connect to the databases
+songs_conn = sqlite3.connect(songs_db_path)
+playlists_conn = sqlite3.connect(playlists_db_path)
 
-def update_track_weight(cursor, track_id):
-    query = """
-        INSERT INTO tracks (track_id, track_count_in_playlist) VALUES (?, 1)
-        ON CONFLICT(track_id) DO UPDATE SET track_count_in_playlist = track_count_in_playlist + 1
-    """
-    cursor.execute(query, (track_id,))
+# Fetch all playlists
+playlists_cursor = playlists_conn.cursor()
+playlists_cursor.execute("SELECT playlist_id FROM playlists")
+playlists = playlists_cursor.fetchall()
 
-
-def delete_empty_playlist_feature(cursor):
-    query = "DELETE FROM playlists WHERE playlist_most_artist_id IS NULL"
-    cursor.execute(query)
+# Helper function to get min and max values from a list of tuples
 
 
-def calculate_track_genres(cursor_songs, track_ids):
-    genres = []
-    for track_id in track_ids:
-        cursor_songs.execute("""
-            SELECT artist_ids FROM tracks WHERE track_id = ?
-        """, (track_id,))
-        artist_ids = cursor_songs.fetchone()
-        if artist_ids:
-            for artist_id in artist_ids[0].split(','):
-                cursor_songs.execute("""
-                    SELECT artist_genres FROM artists WHERE artist_id = ?
-                """, (artist_id,))
-                artist_genres = cursor_songs.fetchone()
-                if artist_genres:
-                    genres.extend(artist_genres[0].split(','))
-    genre_counts = Counter(genres)
-    top_genres = [genre for genre, count in genre_counts.most_common(20)]
-    return ",".join(top_genres)
+def get_min_max(feature_list):
+    return min(feature_list), max(feature_list)
 
 
-def calculate_track_moods(cursor_songs, track_id):
-    cursor_songs.execute("""
-        SELECT acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, valence
-        FROM tracks WHERE track_id = ?
-    """, (track_id,))
-    features = cursor_songs.fetchone()
-    if features:
-        moods = {
-            'happy': features[7],  # valence
-            'energetic': features[2],  # energy
-            'danceable': features[1],  # danceability
-            'acoustic': features[0]  # acousticness
-        }
-        return ",".join([k for k, v in moods.items() if v > 0.5])
-    return ""
+# Process each playlist
+for playlist in playlists:
+    playlist_id = playlist[0]
 
+    # Fetch track_ids for the playlist
+    playlists_cursor.execute(
+        "SELECT playlist_items FROM items WHERE playlist_id=?", (playlist_id,))
+    playlist_items = playlists_cursor.fetchone()
+    if not playlist_items:
+        continue
 
-def fill_playlists_table():
-    with sqlite3.connect(playlists_db_path) as conn_playlists, \
-            sqlite3.connect(songs_db_path) as conn_songs:
+    track_ids = playlist_items[0].split(',')
 
-        cursor_playlists = conn_playlists.cursor()
-        cursor_songs = conn_songs.cursor()
+    # Fetch all audio features for tracks in this playlist
+    placeholders = ','.join('?' for _ in track_ids)
+    songs_cursor = songs_conn.cursor()
+    songs_cursor.execute(f"SELECT duration_ms, popularity, acousticness, danceability, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, time_signature, valence FROM tracks WHERE track_id IN ({
+                         placeholders})", track_ids)
+    track_features = songs_cursor.fetchall()
 
-        cursor_playlists.execute(
-            "SELECT playlist_id, playlist_items FROM items")
-        playlists = cursor_playlists.fetchall()
+    if not track_features:
+        continue
 
-        for playlist_id, playlist_items in playlists:
-            track_ids = playlist_items.split(',')
+    # Calculate min and max for each feature
+    duration_ms = [track[0] for track in track_features]
+    popularity = [track[1] for track in track_features]
+    acousticness = [track[2] for track in track_features]
+    danceability = [track[3] for track in track_features]
+    energy = [track[4] for track in track_features]
+    instrumentalness = [track[5] for track in track_features]
+    key = [track[6] for track in track_features]
+    liveness = [track[7] for track in track_features]
+    loudness = [track[8] for track in track_features]
+    mode = [track[9] for track in track_features]
+    speechiness = [track[10] for track in track_features]
+    tempo = [track[11] for track in track_features]
+    time_signature = [track[12] for track in track_features]
+    valence = [track[13] for track in track_features]
 
-            min_max_values = {
-                'duration_ms': [],
-                'popularity': [],
-                'acousticness': [],
-                'danceability': [],
-                'energy': [],
-                'instrumentalness': [],
-                'key': [],
-                'liveness': [],
-                'loudness': [],
-                'mode': [],
-                'speechiness': [],
-                'tempo': [],
-                'time_signature': [],
-                'valence': [],
-                'artist_ids': [],
-                'genres': [],
-                'moods': []
-            }
+    min_duration_ms, max_duration_ms = get_min_max(duration_ms)
+    min_popularity, max_popularity = get_min_max(popularity)
+    min_acousticness, max_acousticness = get_min_max(acousticness)
+    min_danceability, max_danceability = get_min_max(danceability)
+    min_energy, max_energy = get_min_max(energy)
+    min_instrumentalness, max_instrumentalness = get_min_max(instrumentalness)
+    min_key, max_key = get_min_max(key)
+    min_liveness, max_liveness = get_min_max(liveness)
+    min_loudness, max_loudness = get_min_max(loudness)
+    min_mode, max_mode = get_min_max(mode)
+    min_speechiness, max_speechiness = get_min_max(speechiness)
+    min_tempo, max_tempo = get_min_max(tempo)
+    min_time_signature, max_time_signature = get_min_max(time_signature)
+    min_valence, max_valence = get_min_max(valence)
 
-            for track_id in track_ids:
-                cursor_songs.execute("""
-                    SELECT duration_ms, popularity, acousticness, danceability, energy,
-                    instrumentalness, key, liveness, loudness, mode,
-                    speechiness, tempo, time_signature, valence, artist_ids
-                    FROM tracks
-                    WHERE track_id = ?
-                """, (track_id,))
-                track_info = cursor_songs.fetchone()
+    # Update playlists table with calculated values
+    playlists_cursor.execute("""
+        UPDATE playlists
+        SET min_duration_ms=?, max_duration_ms=?, min_popularity=?, max_popularity=?,
+            min_acousticness=?, max_acousticness=?, min_danceability=?, max_danceability=?,
+            min_energy=?, max_energy=?, min_instrumentalness=?, max_instrumentalness=?,
+            min_key=?, max_key=?, min_liveness=?, max_liveness=?, min_loudness=?, max_loudness=?,
+            min_mode=?, max_mode=?, min_speechiness=?, max_speechiness=?, min_tempo=?, max_tempo=?,
+            min_time_signature=?, max_time_signature=?, min_valence=?, max_valence=?
+        WHERE playlist_id=?
+    """, (
+        min_duration_ms, max_duration_ms, min_popularity, max_popularity,
+        min_acousticness, max_acousticness, min_danceability, max_danceability,
+        min_energy, max_energy, min_instrumentalness, max_instrumentalness,
+        min_key, max_key, min_liveness, max_liveness, min_loudness, max_loudness,
+        min_mode, max_mode, min_speechiness, max_speechiness, min_tempo, max_tempo,
+        min_time_signature, max_time_signature, min_valence, max_valence,
+        playlist_id
+    ))
 
-                if track_info:
-                    (duration_ms, popularity, acousticness, danceability, energy,
-                     instrumentalness, key, liveness, loudness, mode,
-                     speechiness, tempo, time_signature, valence, artist_ids) = track_info
-
-                    min_max_values['duration_ms'].append(duration_ms)
-                    min_max_values['popularity'].append(popularity)
-                    min_max_values['acousticness'].append(acousticness)
-                    min_max_values['danceability'].append(danceability)
-                    min_max_values['energy'].append(energy)
-                    min_max_values['instrumentalness'].append(instrumentalness)
-                    min_max_values['key'].append(key)
-                    min_max_values['liveness'].append(liveness)
-                    min_max_values['loudness'].append(loudness)
-                    min_max_values['mode'].append(mode)
-                    min_max_values['speechiness'].append(speechiness)
-                    min_max_values['tempo'].append(tempo)
-                    min_max_values['time_signature'].append(time_signature)
-                    min_max_values['valence'].append(valence)
-                    min_max_values['artist_ids'].extend(artist_ids.split(','))
-
-                    # Fetch genres for each artist in the track
-                    for artist_id in artist_ids.split(','):
-                        cursor_songs.execute(
-                            "SELECT artist_genres FROM artists WHERE artist_id = ?", (artist_id,))
-                        artist_genres = cursor_songs.fetchone()
-                        if artist_genres:
-                            min_max_values['genres'].extend(
-                                artist_genres[0].split(','))
-
-                    # Calculate track moods and add to min_max_values
-                    track_moods = calculate_track_moods(cursor_songs, track_id)
-                    if track_moods:
-                        min_max_values['moods'].extend(track_moods.split(','))
-
-                    # Update track weight
-                    update_track_weight(cursor_playlists, track_id)
-
-            if min_max_values['duration_ms']:
-                # Get the most common artist ID
-                most_artist_id = max(
-                    set(min_max_values['artist_ids']), key=min_max_values['artist_ids'].count)
-
-                # Get the top 5 most common genres
-                genre_counts = Counter(min_max_values['genres'])
-                most_genres = ",".join(
-                    [genre for genre, count in genre_counts.most_common(5)])
-
-                # Get the top 3 most common moods
-                mood_counts = Counter(min_max_values['moods'])
-                most_moods = ",".join(
-                    [mood for mood, count in mood_counts.most_common(3)])
-
-                cursor_playlists.execute("""
-                    UPDATE playlists SET
-                        min_duration_ms = ?, max_duration_ms = ?, min_popularity = ?, max_popularity = ?, 
-                        min_acousticness = ?, max_acousticness = ?, min_danceability = ?, max_danceability = ?, 
-                        min_energy = ?, max_energy = ?, min_instrumentalness = ?, max_instrumentalness = ?, 
-                        min_key = ?, max_key = ?, min_liveness = ?, max_liveness = ?, 
-                        min_loudness = ?, max_loudness = ?, min_mode = ?, max_mode = ?, 
-                        min_speechiness = ?, max_speechiness = ?, min_tempo = ?, max_tempo = ?, 
-                        min_time_signature = ?, max_time_signature = ?, min_valence = ?, max_valence = ?, 
-                        playlist_most_artist_id = ?, playlist_most_genres = ?, playlist_most_moods = ?, 
-                        fetched_track_count = ?
-                    WHERE playlist_id = ?
-                """, (
-                    min(min_max_values['duration_ms']), max(
-                        min_max_values['duration_ms']),
-                    min(min_max_values['popularity']), max(
-                        min_max_values['popularity']),
-                    min(min_max_values['acousticness']), max(
-                        min_max_values['acousticness']),
-                    min(min_max_values['danceability']), max(
-                        min_max_values['danceability']),
-                    min(min_max_values['energy']), max(
-                        min_max_values['energy']),
-                    min(min_max_values['instrumentalness']), max(
-                        min_max_values['instrumentalness']),
-                    min(min_max_values['key']), max(min_max_values['key']),
-                    min(min_max_values['liveness']), max(
-                        min_max_values['liveness']),
-                    min(min_max_values['loudness']), max(
-                        min_max_values['loudness']),
-                    min(min_max_values['mode']), max(min_max_values['mode']),
-                    min(min_max_values['speechiness']), max(
-                        min_max_values['speechiness']),
-                    min(min_max_values['tempo']), max(min_max_values['tempo']),
-                    min(min_max_values['time_signature']), max(
-                        min_max_values['time_signature']),
-                    min(min_max_values['valence']), max(
-                        min_max_values['valence']),
-                    most_artist_id, most_genres, most_moods, len(
-                        track_ids), playlist_id
-                ))
-            else:
-                print(f"No track information found for playlist {playlist_id}")
-
-        delete_empty_playlist_feature(cursor_playlists)
-        conn_playlists.commit()
-
-
-def fill_tracks_table():
-    with sqlite3.connect(songs_db_path) as conn_songs, \
-            sqlite3.connect(playlists_db_path) as conn_playlists:
-
-        cursor_songs = conn_songs.cursor()
-        cursor_playlists = conn_playlists.cursor()
-
-        cursor_songs.execute("SELECT track_id FROM tracks")
-        tracks = cursor_songs.fetchall()
-
-        for track_id, in tracks:
-            cursor_playlists.execute("""
-                SELECT playlist_items FROM items WHERE playlist_items LIKE ?
-            """, (f'%{track_id}%',))
-            playlists_containing_track = cursor_playlists.fetchall()
-
-            if playlists_containing_track:
-                playlist_ids = [playlist_items.split(
-                    ',') for playlist_items, in playlists_containing_track]
-                flattened_playlist_ids = [
-                    item for sublist in playlist_ids for item in sublist]
-                track_genres = calculate_track_genres(
-                    cursor_songs, flattened_playlist_ids)
-                track_moods = calculate_track_moods(cursor_songs, track_id)
-
-                cursor_songs.execute("""
-                    UPDATE tracks SET
-                        track_count_in_playlist = ?,
-                        track_genres = ?,
-                        track_moods = ?
-                    WHERE track_id = ?
-                """, (len(playlists_containing_track), track_genres, track_moods, track_id))
-
-        conn_songs.commit()
-
-
-if __name__ == "__main__":
-    fill_playlists_table()
-    fill_tracks_table()
-    print("DONE")
+# Commit the changes and close the connections
+playlists_conn.commit()
+playlists_conn.close()
+songs_conn.close()
