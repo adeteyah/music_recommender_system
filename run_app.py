e5f88fa68ad12413c0d18985523b9d1226a17224
Adeteyah <adeteyah@gmail.com>
2024-08-06 18:55:51 +0000

    Aug 7, 2024, 1:55 AM

--- run_app.py
+++ run_app.py
@@ -10,32 +10,23 @@ n_separator = 20
 ids = ['6T7MAQCekVb3UnCykjX3BP',
        '0v1XpBHnsbkCn7iJ9Ucr1l', '1BLfQ6dPXmuDrFmbdfW7Jl']
 
-print('\n', "- " * n_separator, '\n')
-
 start = time.time()
 cf.cf(ids)
 end = time.time()
 print("Execution time: ", end - start)
-
-print('\n', "- " * n_separator, '\n')
 
 # start = time.time()
 # cf_cbf.cfcbf(ids)
 # end = time.time()
 # print("Execution time: ", end - start)
 
-# print('\n', "- " * n_separator, '\n')
 
 start = time.time()
 cbf.cbf(ids)
 end = time.time()
 print("Execution time: ", end - start)
 
-print('\n', "- " * n_separator, '\n')
-
 # start = time.time()
 # cbf_cf.cbfcf(ids)
 # end = time.time()
 # print("Execution time: ", end - start)
-
-# print('\n', "- " * n_separator, '\n')
