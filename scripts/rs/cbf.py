214606840e0ef83a4efa72fe217b26e7cd6ee020
Adeteyah <adeteyah@gmail.com>
2024-08-06 18:30:37 +0000

    Aug 7, 2024, 1:30 AM

--- scripts/rs/cbf.py
+++ scripts/rs/cbf.py
@@ -9,26 +9,27 @@ MODEL = 'Content-based Filtering'
 DB = config['rs']['db_path']
 OUTPUT_PATH = config['rs']['cbf_output']
 N_RESULT = int(config['rs']['n_result'])
+CBF_FEATURES = config['rs']['cbf_features'].split(', ')
 
 
-def get_song_info(conn, song_id):
-    cursor = conn.cursor()
-    cursor.execute("""
+def get_song_info(conn, song_id, features):
+    features_sql = ', '.join(features)
+    query = f"""
         SELECT s.song_id, s.song_name, s.artist_ids, a.artist_name, a.artist_genres,
-               s.acousticness, s.danceability, s.energy, s.instrumentalness,
-               s.key, s.liveness, s.loudness, s.mode, s.speechiness,
-               s.tempo, s.time_signature, s.valence
+               {features_sql}
         FROM songs s
         JOIN artists a ON s.artist_ids = a.artist_id
         WHERE s.song_id = ?
-    """, (song_id,))
+    """
+    cursor = conn.cursor()
+    cursor.execute(query, (song_id,))
     return cursor.fetchone()
 
 
-def read_inputted_ids(ids, conn):
+def read_inputted_ids(ids, conn, features):
     song_info_list = []
     for song_id in ids:
-        song_info = get_song_info(conn, song_id)
+        song_info = get_song_info(conn, song_id, features)
         if song_info:
             song_info_list.append(song_info)
     return song_info_list
@@ -36,31 +37,24 @@ def read_inputted_ids(ids, conn):
 
 def cbf(ids):
     conn = sqlite3.connect(DB)
-    songs_info = read_inputted_ids(ids, conn)
+    features = ['s.' + feature for feature in CBF_FEATURES]
+    songs_info = read_inputted_ids(ids, conn, features)
 
     with open(OUTPUT_PATH, 'w', encoding='utf-8') as f:
         f.write('INPUTTED IDS\n')
         for idx, song_info in enumerate(songs_info, start=1):
-            (song_id, song_name, artist_ids, artist_name, artist_genres,
-             acousticness, danceability, energy, instrumentalness,
-             key, liveness, loudness, mode, speechiness,
-             tempo, time_signature, valence) = song_info
+            # song_id, song_name, artist_ids, artist_name, artist_genres
+            base_info = song_info[:5]
+            audio_features = song_info[5:]
 
+            song_id, song_name, artist_ids, artist_name, artist_genres = base_info
             song_url = f"https://open.spotify.com/track/{song_id}"
+            features_str = ', '.join(
+                [f"{CBF_FEATURES[i]}: {audio_features[i]}" for i in range(len(audio_features))])
+
             line = (f"{idx}. {song_url} {artist_name} - {song_name} | "
                     f"Genres: {artist_genres} | "
-                    f"acousticness: {acousticness}, "
-                    f"danceability: {danceability}, "
-                    f"energy: {energy}, "
-                    f"instrumentalness: {instrumentalness}, "
-                    f"key: {key}, "
-                    f"liveness: {liveness}, "
-                    f"loudness: {loudness}, "
-                    f"mode: {mode}, "
-                    f"speechiness: {speechiness}, "
-                    f"tempo: {tempo}, "
-                    f"time_signature: {time_signature}, "
-                    f"valence: {valence}\n")
+                    f"{features_str}\n")
             f.write(line)
 
     conn.close()
