651420a216d31f586262fc87c36c716144f918ee
Adeteyah <adeteyah@gmail.com>
2024-08-09 12:46:12 +0000

    Aug 9, 2024, 7:46 PM

--- scripts/tools/get_ids.py
+++ scripts/tools/get_ids.py
@@ -1,9 +1,7 @@
 import re
 
 text = """
-https://open.spotify.com/playlist/7561pRhGID1JY9iylnjTeC?si=01162454632345ef  https://open.spotify.com/playlist/5FIczQXxvrVuqiLT8zIw5f?si=a4c6aece05e9491a https://open.spotify.com/playlist/49Jw0JbNJexKQGo9KpPGsO?si=765b3abf853e48c2 https://open.spotify.com/playlist/0EWoCeUOQAEEIVmIkfpMP2?si=94d4839e50264d07 https://open.spotify.com/playlist/0SgyK6Rl58KJH3UGWDhSyc?si=a3a2237b16924a37 https://open.spotify.com/playlist/6uLsdWljLVNjLuPRlehDt4?si=92e0bec200c44dab https://open.spotify.com/playlist/3t9g0zGUYvn91KlEYeg8CN?si=688c3a172bf5409f https://open.spotify.com/playlist/1BJjVFVmV6kdXDZZpwaxSK?si=eea87ab202094277 https://open.spotify.com/playlist/3AUAs41Vr5wcwbGHq9ksHH?si=dd4d58e50ef94b19 https://open.spotify.com/playlist/49Ek1T3Hlr3Znwx8qHHg5g?si=a3690838cc794fb8 https://open.spotify.com/playlist/0SgyK6Rl58KJH3UGWDhSyc?si=297a40a0d47f479d
-
-
+https://open.spotify.com/playlist/4wd6242y2qn44TiCCPX12W?si=1b110b8722bd47c1
 
 
 
