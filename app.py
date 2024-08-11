cd5a2e2c67a212716bb747b8f74127adfe7a014e
Adeteyah <adeteyah@gmail.com>
2024-08-11 04:58:11 +0000

    Aug 11, 2024, 11:58 AM

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from scripts.rs import cbf_cf
 ids = ['3wlLknnMtD8yZ0pCtCeeK4', '6EIMUjQ7Q8Zr2VtIUik4He',
        '30Z12rJpW0M0u8HMFpigTB']
 
-start = time.time()
-cf.cf(ids)
-end = time.time()
-print("Time: ", end - start)
+# start = time.time()
+# cf.cf(ids)
+# end = time.time()
+# print("Time: ", end - start)
 
 # start = time.time()
 # cf_cbf.cf_cbf(ids)
