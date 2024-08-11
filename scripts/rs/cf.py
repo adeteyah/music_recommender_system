39c88eb008f4c7b3ba497b7989e12cc54936c34a
Adeteyah <adeteyah@gmail.com>
2024-08-11 12:57:25 +0000

    Aug 11, 2024, 7:57 PM

--- scripts/rs/cf.py
+++ scripts/rs/cf.py
@@ -13,16 +13,42 @@ OUTPUT_PATH = config['rs']['cf_output']
 
 def get_song_info(conn, song_id):
     cursor = conn.cursor()
+
+    # Query to get song details
     cursor.execute("""
-        SELECT s.song_id, s.song_name, s.artist_ids, a.artist_name, a.artist_genres,
+        SELECT s.song_id, s.song_name, s.artist_ids, 
                s.acousticness, s.danceability, s.energy, s.instrumentalness, 
                s.key, s.liveness, s.loudness, s.mode, s.speechiness, s.tempo, 
                s.time_signature, s.valence
         FROM songs s
-        JOIN artists a ON s.artist_ids = a.artist_id
         WHERE s.song_id = ?
     """, (song_id,))
-    return cursor.fetchone()
+    song_details = cursor.fetchone()
+
+    if not song_details:
+        return None
+
+    # Extract the artist_ids and use only the first one
+    song_id, song_name, artist_ids, acousticness, danceability, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, time_signature, valence = song_details
+    first_artist_id = artist_ids.split(',')[0]  # Get the first artist ID
+
+    # Query to get artist details
+    cursor.execute("""
+        SELECT artist_name, artist_genres
+        FROM artists
+        WHERE artist_id = ?
+    """, (first_artist_id,))
+    artist_details = cursor.fetchone()
+
+    if not artist_details:
+        return None
+
+    artist_name, artist_genres = artist_details
+
+    return (song_id, song_name, artist_ids, artist_name, artist_genres,
+            acousticness, danceability, energy, instrumentalness, key,
+            liveness, loudness, mode, speechiness, tempo, time_signature,
+            valence)
 
 
 def get_playlists_for_song(conn, song_id):
@@ -51,9 +77,20 @@ def get_songs_from_playlists(conn, playlist_ids):
 
 
 def format_song_info(song_info, count=None):
-    song_id, song_name, artist_ids, artist_name, artist_genres, acousticness, danceability, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, time_signature, valence = song_info
-    base_info = f"https://open.spotify.com/track/{song_id} {artist_name} - {song_name} | Genre: {artist_genres} | Acousticness: {acousticness}, Danceability: {danceability}, Energy: {energy}, Instrumentalness: {
-        instrumentalness}, Key: {key}, Liveness: {liveness}, Loudness: {loudness}, Mode: {mode}, Speechiness: {speechiness}, Tempo: {tempo}, Time Signature: {time_signature}, Valence: {valence}"
+    (song_id, song_name, artist_ids, artist_name, artist_genres, acousticness,
+     danceability, energy, instrumentalness, key, liveness, loudness, mode,
+     speechiness, tempo, time_signature, valence) = song_info
+
+    base_info = (f"https://open.spotify.com/track/{song_id} {artist_name} - {song_name} | "
+                 f"Genre: {artist_genres} | Acousticness: {
+                     acousticness}, Danceability: {danceability}, "
+                 f"Energy: {energy}, Instrumentalness: {
+                     instrumentalness}, Key: {key}, "
+                 f"Liveness: {liveness}, Loudness: {loudness}, Mode: {mode}, "
+                 f"Speechiness: {speechiness}, Tempo: {
+                     tempo}, Time Signature: {time_signature}, "
+                 f"Valence: {valence}")
+
     return base_info + (f" | COUNT: {count}" if count is not None else "")
 
 
