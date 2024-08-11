4ada29207b6e48fa0c7a155daad18620a22c2b5e
Adeteyah <adeteyah@gmail.com>
2024-08-11 00:23:40 +0000

    Aug 11, 2024, 7:23 AM

--- scripts/tools/scrape_id.py
+++ scripts/tools/scrape_id.py
@@ -3,7 +3,7 @@ import re
 text = """
 https://open.spotify.com/playlist/37i9dQZF1DZ06evO4s4B1e https://open.spotify.com/playlist/2FnzHLgRojDJSQCmMwOy0O https://open.spotify.com/playlist/17mIXPwdLS4piVL3OzSirt
 
-
+https://open.spotify.com/playlist/5lubzlfVf4fnCyhDtGiVtC?si=c9740e47ad894e35
 
 
 
