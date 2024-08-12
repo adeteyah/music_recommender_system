d5a0b98639f72f736303be141726259c856ce338
Adeteyah <adeteyah@gmail.com>
2024-08-12 07:18:21 +0000

    Aug 12, 2024, 2:18 PM

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from scripts.rs import cf_cbf
 from scripts.rs import cbf_optimized as cbf
 from scripts.rs import cbf_cf
 
-ids = ['1yKAqZoi8xWGLCf5vajroL',
-       '5VGlqQANWDKJFl0MBG3sg2', '0lP4HYLmvowOKdsQ7CVkuq']
+ids = ['2kJwzbxV2ppxnQoYw4GLBZ', '5dn6QANKbf76pANGjMBida',
+       '3Sbova9DAY3pc9GTAACT4b', '5O2P9iiztwhomNh8xkR9lJ', '4pvb0WLRcMtbPGmtejJJ6y']
 
 start = time.time()  # DONE
 cf.cf(ids)
