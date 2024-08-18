e91f5b5268425878d9783fef2f16b1012dcb2a96
Adeteyah <adeteyah@gmail.com>
2024-08-18 18:54:19 +0000

    Aug 19, 2024, 1:54 AM

--- scripts/tools/scrape_playlist.py
+++ scripts/tools/scrape_playlist.py
@@ -18,8 +18,8 @@ CLIENT_ID = config['api']['client_id']
 CLIENT_SECRET = config['api']['client_secret']
 DELAY_TIME = float(config['scrape']['delay_time'])
 
-IDS = ['1wH2w17ULzCMqHh4T8PIa0', '37i9dQZF1DX3ORvAIr1RbU', '3M7RT85xu5DQ4MjXp7D8CA', '3oqNWDNayfTg8LkVl0oDFB', '5PAUOGIn9owQHQIVSO5UJC', '6BBChmyEHeCcx1gdcfG77H', '2SVKEnKxbwdG8GqeWi1g6w', '5TQ9IymI6oFG9Gm0YiTQi9', '1iCzA0f7Prp6C9ap5mRYvt', '51HBHUPs5qowNxm0WCd3hO', '7EzOz99Q8ydRsQSnkPUHCK', '4whrH8VBkbHSfOYRK4i7i0', '2IlNa6S6Nqd9NfQfJ0DECL', '0zPbEMxveRMhnFxc9MqaDn', '1OUPDUQABebnhzCAKGHXTw',
-       '5tM9Ivg3zq0GTYddDiqfkU', '4WVLVRAdEEPY0EwcK32XMp', '5wYznTRPm4DPiWasExVN5p', '4ldNqfCjP1cFbULSkv2tfb', '6QDNdalbzdN45kyHx4upLM', '62KGJKfY57DriYRePa97gC', '37i9dQZF1DXbEm2sKzgoJ8', '71G9jK3syzeqiRt77ihxJ0', '5Ph7YD116r7cWdJBEPY0TI', '5pRm1Urao5hO6YNcIxvVZX', '5zeiXWuSLl4tVhJ1KO1RoY', '37i9dQZF1DWXBcLLksEQAf', '0YQ3cd7KqFSDtczetJqvyG', '2dHb0aB8IQEDTxqymWqvfE', '7xhNTdxImpSdV1aGsNLlOn']
+IDS = ['6V74xRl4hYX8cmLT7fywrH', '77umv1bBpc8J1LwgF6Z0Zv', '7xBH6HAUcaxLpAK5xv0Gso', '6n4xdTisubdIYrafVyoSqt', '0ql0RY0eQbWhm2T4XOnjQ1', '4A4CfD3GgVYyDyKpk7RjJh', '0HmEO6swrbowZcizagWzo7', '57V7j9vt18lxdRZBthgnUK', '4ccjnypUiBUKHGKGaDuATg', '2kaDfCdhq6I572Q3nT1mLS',
+       '18CRyViH5bqzWv8kFCIoKX', '13tlYfrCfnY3PODpWTO28z', '37i9dQZF1DZ06evO2QTIoV', '0QO2mLVcs1OsKF3kVWpRN5', '0xfFh3DIITXFV3DQzCSTrh', '1yNXopoGHXhYKvnF9ghW6r', '2Z1EcZkCxwVXomQ14VFpdq', '0yMHdtmr7aI2rAW3ZNxvZa', '6wanVv6EsTV8lEnGTldOzb', '2S6XgJze3XveR81Nh965YP']
 
 
 def init_spotify(client_id, client_secret):
