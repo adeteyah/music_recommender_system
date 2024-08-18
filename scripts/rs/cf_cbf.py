3efc72318d034b9a4d70e0f185c0ac44914cd55d
Adeteyah <adeteyah@gmail.com>
2024-08-18 21:01:25 +0000

    Aug 19, 2024, 4:01 AM

--- scripts/rs/cf_cbf.py
+++ scripts/rs/cf_cbf.py
@@ -6,11 +6,12 @@ from collections import defaultdict
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
+# Constants
 MODEL = 'Collaborative Filtering + Content-based Filtering'
 DB = config['rs']['db_path']
 OUTPUT_PATH = config['rs']['cf_cbf_output']
 
-# Read and parse the features used for Content-based Filtering
+# Feature and Bound Values
 FEATURE_SELECT = config['hp']['cf_cbf_features'].split(', ')
 REAL_BOUND_VAL = float(config['hp']['cf_cbf_real_bound'])
 MODE_BOUND_VAL = int(config['hp']['cf_cbf_mode_bound'])
@@ -24,6 +25,14 @@ SONGS_PER_ARTIST = int(config['hp']['songs_per_artist'])
 GENRE_WEIGHT = 7
 AF_WEIGHT = 0.2
 COUNT_WEIGHT = 0.1
+
+
+def get_db_connection(db_path):
+    try:
+        return sqlite3.connect(db_path)
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+        return None
 
 
 def get_song_info(conn, song_id):
@@ -44,7 +53,7 @@ def get_song_info(conn, song_id):
         return None
 
     song_id, song_name, artist_ids, acousticness, danceability, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, time_signature, valence = song_details
-    first_artist_id = artist_ids.split(',')[0]  # Get the first artist ID
+    first_artist_id = artist_ids.split(',')[0]
 
     # Query to get artist details
     cursor.execute("""
@@ -58,7 +67,6 @@ def get_song_info(conn, song_id):
         return None
 
     artist_name, artist_genres = artist_details
-    # Only keep the first two genres
     top_two_genres = ','.join(artist_genres.split(',')[:2])
 
     return (song_id, song_name, artist_ids, artist_name, top_two_genres,
@@ -79,8 +87,9 @@ def get_playlists_for_song(conn, song_id):
 
 def get_songs_from_playlists(conn, playlist_ids):
     song_counts = defaultdict(int)
+    cursor = conn.cursor()
+
     for playlist_id in playlist_ids:
-        cursor = conn.cursor()
         cursor.execute("""
             SELECT playlist_items
             FROM playlists
@@ -89,6 +98,7 @@ def get_songs_from_playlists(conn, playlist_ids):
         items = cursor.fetchone()[0].split(',')
         for song_id in items:
             song_counts[song_id] += 1
+
     return song_counts
 
 
@@ -139,8 +149,7 @@ def calculate_similarity(song_info, input_song_info):
     return similarity_score
 
 
-def is_similar_song(song_info, input_song_info, FEATURE_SELECT):
-    similarity_score = 0
+def is_similar_song(song_info, input_song_info):
     feature_ranges = {
         'acousticness': REAL_BOUND_VAL,
         'danceability': REAL_BOUND_VAL,
@@ -168,10 +177,66 @@ def is_similar_song(song_info, input_song_info, FEATURE_SELECT):
     return True
 
 
-def cf_cbf(ids):
-    conn = sqlite3.connect(DB)
-    songs_info = read_inputted_ids(ids, conn)
+def write_song_info(file, index, song_info):
+    if song_info:
+        formatted_info = format_song_info(song_info)
+        file.write(f"{index}. {formatted_info}\n")
+    else:
+        file.write(f"{index}. Song ID not found in the database.\n")
 
+
+def write_playlist_info(file, playlists):
+    for j, (playlist_id, playlist_creator_id, _) in enumerate(playlists, 1):
+        file.write(f"{j}. https://open.spotify.com/playlist/{playlist_id} by "
+                   f"https://open.spotify.com/user/{playlist_creator_id}\n")
+
+
+def write_recommendations(file, conn, song_info, playlist_ids, input_song_ids, input_song_titles_artists):
+    recommended_songs = get_songs_from_playlists(conn, playlist_ids)
+    weighted_recommendations = []
+
+    for recommended_song_id, count in recommended_songs.items():
+        song_recommendation_info = get_song_info(conn, recommended_song_id)
+        if song_recommendation_info:
+            song_title = song_recommendation_info[1]
+            artist_name = song_recommendation_info[3]
+
+            if recommended_song_id in input_song_ids or (song_title, artist_name) in input_song_titles_artists:
+                continue
+
+            if is_similar_song(song_recommendation_info, song_info):
+                similarity_score = calculate_similarity(
+                    song_recommendation_info, song_info)
+                weighted_score = (GENRE_WEIGHT * (1 if song_recommendation_info[4] == song_info[4] else 0) +
+                                  AF_WEIGHT * (1 / (1 + similarity_score)) +
+                                  COUNT_WEIGHT * count)
+                weighted_recommendations.append(
+                    (recommended_song_id, weighted_score, count))
+
+    sorted_recommendations = sorted(
+        weighted_recommendations, key=lambda x: x[1], reverse=True)
+
+    artist_song_count = defaultdict(int)
+    k = 1
+    for recommended_song_id, _, count in sorted_recommendations:
+        song_recommendation_info = get_song_info(conn, recommended_song_id)
+        if song_recommendation_info:
+            artist_name = song_recommendation_info[3]
+
+            if artist_song_count[artist_name] < SONGS_PER_ARTIST:
+                formatted_recommendation = format_song_info(
+                    song_recommendation_info, count)
+                file.write(f"{k}. {formatted_recommendation}\n")
+                artist_song_count[artist_name] += 1
+                k += 1  # Increment after writing a valid song
+
+
+def cf_cbf(ids):
+    conn = get_db_connection(DB)
+    if not conn:
+        return
+
+    songs_info = read_inputted_ids(ids, conn)
     input_song_ids = set(song_info[0] for song_info in songs_info if song_info)
     input_song_titles_artists = {
         (song_info[1], song_info[3]) for song_info in songs_info if song_info}
@@ -179,77 +244,24 @@ def cf_cbf(ids):
     with open(OUTPUT_PATH, 'w', encoding='utf-8') as file:
         file.write('INPUTTED IDS\n')
         for i, song_info in enumerate(songs_info, 1):
-            if song_info is None:  # Check if the song was found
-                file.write(f"{i}. Song ID not found in the database.\n")
-                continue
+            write_song_info(file, i, song_info)
 
-            formatted_info = format_song_info(song_info)
-            file.write(f"{i}. {formatted_info}\n")
+            if song_info:
+                playlists = get_playlists_for_song(conn, song_info[0])
+                playlist_ids = [playlist_id for playlist_id, _, _ in playlists]
 
-            file.write(f"\nFOUND IN:\n")
-            playlists = get_playlists_for_song(conn, song_info[0])
-            playlist_ids = [playlist_id for playlist_id, _, _ in playlists]
-            for j, (playlist_id, playlist_creator_id, _) in enumerate(playlists, 1):
-                file.write(f"{j}. https://open.spotify.com/playlist/{
-                           playlist_id} by https://open.spotify.com/user/{playlist_creator_id}\n")
+                if playlists:
+                    file.write(f"\nFOUND IN:\n")
+                    write_playlist_info(file, playlists)
 
-            if playlist_ids:
-                file.write(f"\nSONGS RECOMMENDATION: {formatted_info}\n")
-                recommended_songs = get_songs_from_playlists(
-                    conn, playlist_ids)
-
-                weighted_recommendations = []
-                for recommended_song_id, count in recommended_songs.items():
-                    song_recommendation_info = get_song_info(
-                        conn, recommended_song_id)
-                    if song_recommendation_info:
-                        song_title = song_recommendation_info[1]
-                        artist_name = song_recommendation_info[3]
-                        recommended_genres = song_recommendation_info[4].split(
-                            ',')
-
-                        # Check if the recommended song's genres match any of the first two genres of the input song
-                        input_genres = song_info[4].split(',')
-                        if not any(genre in recommended_genres for genre in input_genres):
-                            continue
-
-                        if (recommended_song_id in input_song_ids or
-                                (song_title, artist_name) in input_song_titles_artists):
-                            continue
-
-                        if is_similar_song(song_recommendation_info, song_info, FEATURE_SELECT):
-                            similarity_score = calculate_similarity(
-                                song_recommendation_info, song_info)
-                            weighted_score = (GENRE_WEIGHT * (1 if song_recommendation_info[4] == song_info[4] else 0) +
-                                              AF_WEIGHT * (1 / (1 + similarity_score)) +
-                                              COUNT_WEIGHT * count)
-                            weighted_recommendations.append(
-                                (recommended_song_id, weighted_score, count))
-
-                sorted_recommendations = sorted(
-                    weighted_recommendations, key=lambda x: x[1], reverse=True)
-
-                artist_song_count = defaultdict(int)
-                k = 1
-                for recommended_song_id, _, count in sorted_recommendations:
-                    song_recommendation_info = get_song_info(
-                        conn, recommended_song_id)
-                    if song_recommendation_info:
-                        song_title = song_recommendation_info[1]
-                        artist_name = song_recommendation_info[3]
-
-                        # Only increment k when a valid song is added
-                        if artist_song_count[artist_name] < SONGS_PER_ARTIST:
-                            formatted_recommendation = format_song_info(
-                                song_recommendation_info, count)
-                            file.write(f"{k}. {formatted_recommendation}\n")
-                            artist_song_count[artist_name] += 1
-                            k += 1  # Increment after writing a valid song
+                    file.write(f"\nSONGS RECOMMENDATION:\n")
+                    write_recommendations(
+                        file, conn, song_info, playlist_ids, input_song_ids, input_song_titles_artists)
 
             file.write('\n')
 
     conn.close()
-    print('Result for', MODEL, 'stored at', OUTPUT_PATH)
+    print(f'Result for {MODEL} stored at {OUTPUT_PATH}')
 
 
 if __name__ == "__main__":
