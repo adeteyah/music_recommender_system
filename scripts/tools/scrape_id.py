98022572c6915e9a74bea58c9508bd7b67075f46
Adeteyah <adeteyah@gmail.com>
2024-08-18 21:26:10 +0000

    Aug 19, 2024, 4:26 AM

--- scripts/tools/scrape_id.py
+++ scripts/tools/scrape_id.py
@@ -1,8 +1,10 @@
 import re
 
 text = """
-https://open.spotify.com/playlist/0P3ieWY6Ri33FzP1suHakC?go=1&sp_cid=a4a40a3999a979f7b5cb2bfe719bf5e3&intent=addToLibrary&utm_source=embed_player_v&utm_medium=desktop  https://open.spotify.com/playlist/4jioZ6p2ZdZuNWlkCS5OXa https://open.spotify.com/playlist/55M6sFoow7KjS9tz0CSZxw
-https://open.spotify.com/playlist/5WqHfJDRG5Gmyw65LR8Uqo
+https://open.spotify.com/playlist/18fxVV3YtRFOicZmc4TYrE?autoplay=true
+
+
+
 
 """
 user_pattern = re.compile(
