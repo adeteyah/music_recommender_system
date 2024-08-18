0a9e0f341a31e4ffcef4c9380c20bc68ccc65963
Adeteyah <adeteyah@gmail.com>
2024-08-18 22:16:33 +0000

    Aug 19, 2024, 5:16 AM

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from scripts.rs import cbf_optimized as cbf
 from scripts.rs import cbf_cf
 import scripts.tools.to_print as compile_result
 
-ids = ['4kPlQKwtPrnqLgrmmKFSlA',
-       '03qu1u4hDyepQQi2lNxCka', '0afhq8XCExXpqazXczTSve']
+ids = ['2qSkIjg1o9h3YT9RAgYN75',
+       '6XKEElbr3cWmnUw6a5HRBV', '5N3hjp1WNayUPZrA8kJmJP']
 
 start = time.time()  # DONE
 cf.cf(ids)
