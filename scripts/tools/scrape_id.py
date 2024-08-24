d38b0ac35268ce14f7de750800a6f503cbedfb44
Adeteyah <adeteyah@gmail.com>
2024-08-24 10:55:55 +0000

    Aug 24, 2024, 5:55 PM

--- scripts/tools/scrape_id.py
+++ scripts/tools/scrape_id.py
@@ -1,8 +1,16 @@
 import re
 
 text = """
-https://open.spotify.com/playlist/74zQbndug04JQOSl8idfIA https://open.spotify.com/playlist/6woZFK1svRjzX59N08UwE0 https://open.spotify.com/playlist/3jltelqHnuWPErD0VUkitM
-https://open.spotify.com/playlist/4ROtkOjstuvQ6Rtd3fmXuG?si=7inr_uU_QMqUsBLKRv6Ucw https://open.spotify.com/playlist/1XMcAQ7AgQR2U7NmTKDwq9 https://open.spotify.com/playlist/6VtQR8iKgg4sd48ovbIfef https://open.spotify.com/playlist/6woZFK1svRjzX59N08UwE0 https://open.spotify.com/playlist/1ldFIzTyLgksmjmhikrq0e https://open.spotify.com/playlist/1XMcAQ7AgQR2U7NmTKDwq9 https://open.spotify.com/playlist/4MAg9d3hT7ZxCysc3MUQcA https://open.spotify.com/playlist/4jTSiPeCp86RXXAj37tQqc https://open.spotify.com/playlist/3SppVKB3kfT6rbElEi9Lsy https://open.spotify.com/playlist/3sdumc7gqOViCbmESNXEJv https://open.spotify.com/playlist/7xTDTQ5ZFFA4xig2I4KLx8
+
+
+
+
+
+
+
+
+
+
 
 
 
