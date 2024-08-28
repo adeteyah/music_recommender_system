8bed15174a254d09bf72874dd5f8d0a90ea2fd6e
Adeteyah <adeteyah@gmail.com>
2024-08-28 14:37:39 +0000

    Aug 28, 2024, 9:37 PM

--- scripts/archive/research/count_form.py
+++ scripts/archive/research/count_form.py
@@ -87,100 +87,99 @@ def main(data):
 
 # Example input
 data = """
-xenn, yang punya korelasi sama musik yang di kirim kemarin kasih (v) ya di belakang
+piw, yang punya korelasi sama musik yang di kirim kemarin kasih (v) ya di belakang
 contoh: 1. artis - judul (v)
 
 CBF
 
-Aimer - カタオモイ
-1. Aimer - カタオモイ (v)
-2. Vaundy - 東京フラッシュ 
-3. 이경섭 - Title 허밍 
-4. eldon - Pink cheeks 
-5. ヨルシカ - ブレーメン 
+My Chemical Romance - Disenchanted (v)
+1. Dashboard Confessional - Stolen 
+2. The Script - For the First Time 
+3. Marilyn Manson - They Said That Hell's Not Hot 
+4. ONE OK ROCK - Wherever you are (v)
+5. Sunday (1994) - Tired Boy 
 
-Juicy Luicy - Jemari
-1. HAECHAN - Good Person (2022) 
-2. Nadya Fatira - Penyendiri 
-3. Raisa - Kali Kedua - Acoustic 
-4. Katie - Yang Telah Merelakanmu X Jaga Selalu Hatimu 
-5. Slank - Terbunuh Sepi 
+Bon Jovi - It's My Life (v)
+1. Hippo Campus - Way It Goes 
+2. Marilyn Manson - Cupid Carries A Gun 
+3. Sylvan Esso - Radio 
+4. Twenty One Pilots - We Don't Believe What's on TV (v)
+5. The Cure - The End Of The World 
 
-Lewis Capaldi - Before You Go
-1. Lewis Capaldi - Before You Go (v)
-2. Hawthorne Heights - Niki Fm 
-3. Ada Band - Yang Terbaik Bagimu 
-4. Ada Band - Yang Terbaik Bagimu - Jangan Lupakan Ayah 
-5. Rex Orange County - Apricot Princess 
+Avenged Sevenfold - A Little Piece of Heaven
+1. Avenged Sevenfold - A Little Piece of Heaven (v)
+2. Falling In Reverse - The Drug In Me Is You 
+3. Foo Fighters - DOA (v)
+4. 311 - Whiskey And Wine 
+5. Mike Repic - Alive 
 
 CBF_CF
 
-Aimer - カタオモイ
-1. Aimer - カタオモイ (v)
-2. Vaundy - 東京フラッシュ 
-3. 이경섭 - Title 허밍 
-4. eldon - Pink cheeks 
-5. ヨルシカ - ブレーメン 
+My Chemical Romance - Disenchanted
+1. Dashboard Confessional - Stolen 
+2. The Script - For the First Time 
+3. Marilyn Manson - They Said That Hell's Not Hot 
+4. ONE OK ROCK - Wherever you are (v)
+5. Sunday (1994) - Tired Boy 
 
-Juicy Luicy - Jemari
-1. HAECHAN - Good Person (2022) 
-2. Nadya Fatira - Penyendiri 
-3. Katie - Yang Telah Merelakanmu X Jaga Selalu Hatimu 
-4. Slank - Terbunuh Sepi 
-5. Dere - Rumah 
+Bon Jovi - It's My Life
+1. Hippo Campus - Way It Goes 
+2. Marilyn Manson - Cupid Carries A Gun 
+3. Sylvan Esso - Radio 
+4. Twenty One Pilots - We Don't Believe What's on TV 
+5. The Cure - The End Of The World 
 
-Lewis Capaldi - Before You Go
-1. Lewis Capaldi - Before You Go (v)
-2. Hawthorne Heights - Niki Fm 
-3. Ada Band - Yang Terbaik Bagimu 
-4. Ada Band - Yang Terbaik Bagimu - Jangan Lupakan Ayah 
-5. Rex Orange County - Apricot Princess 
+Avenged Sevenfold - A Little Piece of Heaven
+1. Avenged Sevenfold - A Little Piece of Heaven 
+2. Falling In Reverse - The Drug In Me Is You 
+3. Foo Fighters - DOA 
+4. 311 - Whiskey And Wine 
+5. Mike Repic - Alive 
 
 CF
 
-Aimer - カタオモイ
-1. ヨルシカ - 花に亡霊 
-2. Yuuri - ミズキリ-piano ver.- 
-3. Motohiro Hata - ひまわりの約束 (v)
-4. YOASOBI - たぶん (v)
-5. Yuuri - レオ 
+My Chemical Romance - Disenchanted
+1. AC/DC - Girls Got Rhythm (v)
+2. Three Days Grace - Never Too Late 
+3. Bring Me The Horizon - Doomed (v)
+4. Deftones - Change (In the House of Flies) 
+5. Bring Me The Horizon - sugar honey ice & tea 
 
-Juicy Luicy - Jemari
-1. Hindia - Setengah Tahun Ini 
-2. Juicy Luicy - Tanpa Tergesa (v) 
-3. Lalahuta - 1 2 3 
-4. Hindia - Besok Mungkin Kita Sampai 
-5. Yovie Widianto - Mantan Terindah 
+Bon Jovi - It's My Life
+1. Bring Me The Horizon - Doomed 
+2. Bring Me The Horizon - sugar honey ice & tea 
+3. Bon Jovi - Runaway 
+4. The Cure - Lovesong 
+5. Nirvana - About A Girl 
 
-Lewis Capaldi - Before You Go
-1. James Arthur - Say You Won't Let Go (v)
-2. Freddie Mercury - Love Me Like There's No Tomorrow - Special Edition 
-3. Maroon 5 - Memories (v)
-4. Sam Smith - Stay With Me (v)
-5. Maroon 5 - Memories (v)
+Avenged Sevenfold - A Little Piece of Heaven
+1. Green Day - 21 Guns (v)
+2. The Cure - Boys Don't Cry 
+3. Avenged Sevenfold - So Far Away (v)
+4. Arctic Monkeys - When The Sun Goes Down 
+5. Oasis - Wonderwall (v)
 
 CF_CBF
 
-Aimer - カタオモイ
-1. Fujii Kaze - Matsuri (v)
-2. Ado - ギラギラ 
-3. Mrs. GREEN APPLE - 私は最強 
-4. Tate McRae - stupid 
-5. Daoko - 打上花火 
+My Chemical Romance - Disenchanted
+1. My Chemical Romance - The End. (V)
+2. Oasis - Wonderwall - Remastered 
+3. Paramore - Misery Business (v)
+4. Lord Huron - The Night We Met 
+5. Coldplay - Yellow (v)
 
-Juicy Luicy - Jemari
-1. Juicy Luicy - Kembali Kesepian 
-2. Juicy Luicy - H-5 
-3. Rossa - Aku Bukan Untukmu 
-4. Fatin - Salahkah Aku Terlalu Mencintaimu 
-3. Lewis Capaldi - Before You Go (v)
+Bon Jovi - It's My Life
+1. Green Day - Last Night on Earth (v)
+2. Guns N' Roses - Patience (v)
+3. Aerosmith - I Don't Want to Miss a Thing - From "Armageddon" Soundtrack (v)
+4. Nirvana - Come As You Are 
+5. Queen - Radio Ga Ga - Live Aid (v)
 
-Lewis Capaldi - Before You Go
-1. Lewis Capaldi - Bruises 
-2. Conan Gray - Memories (v)
-3. Niall Horan - This Town (v)
-4. Bazzi - Why 
-5. Lana Del Rey - Summertime Sadness (v)
+Avenged Sevenfold - A Little Piece of Heaven
+1. Avenged Sevenfold - 4:00 AM 
+2. Mr. Big - Wild World - 2009 Remastered Version 
+3. Liam Gallagher - For What It's Worth 
+4. Slipknot - Duality (v)
 """
 
 if __name__ == "__main__":
